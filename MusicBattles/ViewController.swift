31fffd0433111b664ca01aa84e6b01633a982bda
Rodrigo Lara <rodrigo881215@gmail.com>
2020-02-08 13:53:38 -0600

    Initial Commit

---
+++ MusicBattles/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MusicBattles
+//
+//  Created by Rodrigo Lara Ruano on 08/02/20.
+//  Copyright © 2020 Rodrigo Lara Ruano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
